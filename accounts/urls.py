2dd6bed3798b1510f7c21d2f12a4e7c8e4a14a57
ParisaAg <parisaaghaei17@gmail.com>
2025-05-18 16:06:55 +0330

    initial deploy setup

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path,include
+from .views import RegisterView,LoginView,UserProfileView,LogoutView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'), 
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+
---
+++ accounts/views.py
@@ -0,0 +1,70 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+
+
+# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"your account has been successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            response = Response()
+            response.set_cookie(
+                key='access_token',
+                value=str(refresh.access_token),
+                httponly=True,
+                secure=True,  
+                samesite='Lax'
+            )
+            response.data = {
+                "message": 'you are logged in now',
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            }
+            return response
+        else:
+            return Response({"error": "username or password does'nt match correctly"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email,
+        })
+
+
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response({"message":'you are logged out now'}, status=status.HTTP_200_OK)
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+        return response
