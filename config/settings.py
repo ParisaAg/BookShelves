dafa4035ae307fe2867ddca1c47c3ee1093ab18e
ParisaAg <parisaaghaei17@gmail.com>
2025-05-20 15:41:00 +0330

    sett

--- config/settings.py
+++ config/settings.py
@@ -70,12 +70,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     )
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
+
 
 DATABASES = {
     'default': {
