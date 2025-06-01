ce2c424e30d01e2ed43bce1a8665b73d73c6421e
ParisaAg <parisaaghaei17@gmail.com>
2025-06-01 15:46:45 +0330

    SLIDERS problem fixed

--- sliders/serializers.py
+++ sliders/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from .models import SliderImage
 
-
-
 class SliderImageSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
@@ -12,5 +10,5 @@ class SliderImageSerializer(serializers.ModelSerializer):
 
     def get_image(self, obj):
         if obj.image:
-            return obj.image.url  # This returns the full URL
+            return obj.image.build_url()
         return None
