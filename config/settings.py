980c28170de64cbbf223b1b41a7f6fdf7d830cc0
ParisaAg <parisaaghaei17@gmail.com>
2025-06-01 15:23:43 +0330

    SLIDERS problem fixed

--- config/settings.py
+++ config/settings.py
@@ -213,10 +213,10 @@ CLOUDINARY_STORAGE = {
 }
 
 
-# cloudinary.config(
-#     cloud_name = CLOUDINARY_STORAGE['CLOUD_NAME'],
-#     api_key = CLOUDINARY_STORAGE['API_KEY'],
-#     api_secret = CLOUDINARY_STORAGE['API_SECRET'],
-#     secure = True
-# )
+cloudinary.config(
+    cloud_name = CLOUDINARY_STORAGE['CLOUD_NAME'],
+    api_key = CLOUDINARY_STORAGE['API_KEY'],
+    api_secret = CLOUDINARY_STORAGE['API_SECRET'],
+    secure = True
+)
 
