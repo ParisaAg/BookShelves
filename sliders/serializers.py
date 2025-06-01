378f0401e5321cc6ab6afd03310fb1f92cfb60ca
ParisaAg <parisaaghaei17@gmail.com>
2025-06-01 15:22:05 +0330

    SLIDERS problem fixed

--- sliders/serializers.py
+++ sliders/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from .models import SliderImage
 
 class SliderImageSerializer(serializers.ModelSerializer):
-    # image = serializers.SerializerMethodField()
     class Meta:
         model = SliderImage
         fields = ['id', 'title', 'image', 'created_at']
