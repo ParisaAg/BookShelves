ae575e26dd7d10e20c490fda92beb04ce08f4bd0
ParisaAg <parisaaghaei17@gmail.com>
2025-06-09 18:40:03 +0330

    fix storage to use Liara S3

--- config/settings.py
+++ config/settings.py
@@ -226,13 +226,12 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.getenv('LIARA_ENDPOINT_URL')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-#MEDIA_URL = '/media/'
-#git commit -am "temp fix for media"
-#git push liara master
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-MEDIA_ROOT = '/tmp/media'
 
+MEDIA_URL = '/media/'
 
 
