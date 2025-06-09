49522c54872dfcca60198b74e66a993f63f3fee1
ParisaAg <parisaaghaei17@gmail.com>
2025-06-09 19:09:17 +0330

    temp fix for media

--- sliders/models.py
+++ sliders/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 
 class SliderImage(models.Model):
     title = models.CharField(max_length=100, blank=True)
-    image = models.ImageField(upload_to='sliders/')
+    image = models.ImageField(upload_to='test/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
