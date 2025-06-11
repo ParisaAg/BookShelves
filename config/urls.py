65f6baf20c56f759ee1fa602cceb7e023172a9bd
ParisaAg <parisaaghaei17@gmail.com>
2025-06-11 16:42:00 +0330

    create book

--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,12 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')), 
-    path('api/books/', include('books.urls')),
+    path('api/', include('books.urls')),
     path('api/orders/', include('orders.urls')),
     path('api/carts/', include('carts.urls')),
     path('postgresql/', include('postgresql_app.urls')),
     path('api/sliders/', include('sliders.urls')),
-    path('api/', include('reviews.urls')),
+    path('api/reviews/', include('reviews.urls')),
 
 
 ]
