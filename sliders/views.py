0218f7663da103d9bc14602f52a7c14a390a5157
ParisaAg <parisaaghaei17@gmail.com>
2025-06-11 12:37:27 +0330

    temp fix for media

--- sliders/views.py
+++ sliders/views.py
@@ -11,10 +11,10 @@ class SliderImageUploadView(GenericAPIView):
     serializer_class = SliderImageSerializer
     parser_classes = [MultiPartParser, FormParser]
 
-def post(self, request, *args, **kwargs):
-    serializer = self.get_serializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        instance = serializer.instance
-        return Response(self.get_serializer(instance).data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            instance = serializer.instance
+            return Response(self.get_serializer(instance).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
