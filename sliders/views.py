904b905c1538eadf0c8910fcd17ce6354ae93a9b
ParisaAg <parisaaghaei17@gmail.com>
2025-06-11 12:38:31 +0330

    temp fix for media

--- sliders/views.py
+++ sliders/views.py
@@ -1,3 +1,5 @@
+# views.py
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
@@ -11,10 +13,13 @@ class SliderImageUploadView(GenericAPIView):
     serializer_class = SliderImageSerializer
     parser_classes = [MultiPartParser, FormParser]
 
+    # این متد باید داخل کلاس باشد
     def post(self, request, *args, **kwargs):
+        # مطمئن شوید که سریالایزر از نسخه اصلاح شده قبلی است
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True): # raise_exception=True برای دیباگ بهتر است
             serializer.save()
             instance = serializer.instance
             return Response(self.get_serializer(instance).data, status=status.HTTP_201_CREATED)
+        # با وجود raise_exception=True این خط دیگر لازم نیست، اما بودنش ضرری ندارد
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
