cbf52d8e8a1cbe4a36ff40b656feb3d6b1420fa8
ParisaAg <parisaaghaei17@gmail.com>
2025-07-01 16:37:45 +0330

    urls path  banner and announcement

--- extras/urls.py
+++ extras/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path,include
 from .views import BannerListView,AnnouncementListView
 
-
+urlpatterns = [
 path('banners/', BannerListView.as_view()),
 path('announcements/', AnnouncementListView.as_view()),
+]
