4468b2305ef936acdc53fbc2e9f77488b51a57ec
ParisaAg <parisaaghaei17@gmail.com>
2025-07-02 16:03:57 +0330

    add post method into views

--- extras/admin.py
+++ extras/admin.py
@@ -1,3 +1,7 @@
+
 from django.contrib import admin
+from .models import Banner, Announcement
 
 # Register your models here.
+admin.site.register(Banner)
+admin.site.register(Announcement)
--- extras/views.py
+++ extras/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from .models import Banner, Announcement
 from .serializers import BannerSerializer, AnnouncementSerializer
 
-class BannerListView(ListAPIView):
+class BannerListView(ListCreateAPIView):
     queryset = Banner.objects.filter(is_active=True).order_by('-created_at')
     serializer_class = BannerSerializer
 
-class AnnouncementListView(ListAPIView):
+class AnnouncementListView(ListCreateAPIView):
     queryset = Announcement.objects.all().order_by('-created_at')
     serializer_class = AnnouncementSerializer
