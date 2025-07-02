526fed61a9d39bd47a5cf7e9b76ae168c8fc83d3
ParisaAg <parisaaghaei17@gmail.com>
2025-07-02 04:59:58 +0330

    image-url add to serializer

--- extras/serializers.py
+++ extras/serializers.py
@@ -3,14 +3,19 @@ from rest_framework import serializers
 from .models import Banner,Announcement
 
 class BannerSerializer(serializers.ModelSerializer):
-    class Meta:
+   class Meta:
         model = Banner
         fields = [
-            'id', 'title', 'subtitle', 'image_url', 
+            'id', 'title', 'subtitle', 
+            'image_url', # Now this will use our get_image_url method
             'button_text', 'button_link', 
             'gradient_from', 'gradient_to'
         ]
 
+def get_image_url(self, obj):
+    if obj.image_url and hasattr(obj.image_url, 'url'):
+        return obj.image_url.url
+    return None
 
 class AnnouncementSerializer(serializers.ModelSerializer):
     class Meta:
