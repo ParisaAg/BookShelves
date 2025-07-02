ca9b067d558a9e4feac0466dc7792860153294d5
ParisaAg <parisaaghaei17@gmail.com>
2025-07-02 16:31:45 +0330

    add post method into views

--- extras/serializers.py
+++ extras/serializers.py
@@ -4,21 +4,25 @@ from rest_framework import serializers
 from .models import Banner, Announcement
 
 class BannerSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
+    image = serializers.ImageField(write_only=True, required=False)
+
+    image_url = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Banner
         fields = [
             'id', 'title', 'subtitle',
+            'image',  
             'image_url',  
             'button_text', 'button_link',
             'gradient_from', 'gradient_to'
         ]
 
     
+
     def get_image_url(self, obj):
-        if obj.image_url and hasattr(obj.image_url, 'url'):
-            return obj.image_url.url
+        if obj.image and hasattr(obj.image, 'url'):
+            return obj.image.url
         return None
 
 class AnnouncementSerializer(serializers.ModelSerializer):
