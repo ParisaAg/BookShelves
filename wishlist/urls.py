531f2950319f390d0f6f9fc16db2e2e820bbf697
ParisaAg <parisaaghaei17@gmail.com>
2025-07-14 00:08:36 +0330

    change wishlist urls

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,9 @@
+
 from rest_framework.routers import DefaultRouter
 from .views import WishlistViewSet
 
 router = DefaultRouter()
+
 router.register(r'wishlist', WishlistViewSet, basename='wishlist')
 
 urlpatterns = router.urls
