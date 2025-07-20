b3bc03132f7837cdbba97356d1c2305cc9b19739
ParisaAg <parisaaghaei17@gmail.com>
2025-07-20 17:31:09 +0330

    change contatct admin in  extra

--- extra/admin.py
+++ extra/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Banner, Announcement, ContactMessage
+from .models import Banner, Announcement
 
 @admin.register(Banner)
 class BannerAdmin(admin.ModelAdmin):
@@ -9,9 +9,3 @@ class BannerAdmin(admin.ModelAdmin):
 @admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ('message', 'link_announce', 'created_at')
-
-@admin.register(ContactMessage)
-class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'name', 'email', 'is_read', 'created_at')
-    list_filter = ('is_read', 'created_at')
-    readonly_fields = ('name', 'email', 'subject', 'message', 'created_at')
