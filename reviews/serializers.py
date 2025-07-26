3a9731de42b45c98196ff7dfdf54399f96935c0e
ParisaAg <parisaaghaei17@gmail.com>
2025-07-26 14:24:37 +0330

    add min value error for rating book

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,4 +1,3 @@
-# reviews/serializers.py
 from rest_framework import serializers
 from .models import Review
 
@@ -10,5 +9,10 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'rating', 'comment', 'created_at']
         
         extra_kwargs = {
-            'rating': {'min_value': 1, 'max_value': 5}
+            'rating': {'min_value': 1, 'max_value': 5,
+                    'error_messages': {
+                    'min_value': 'امتیاز باید حداقل ۱ ستاره باشد.', 
+                    'max_value': 'امتیاز باید حداکثر ۵ ستاره باشد.'
+                }
+                       }
         }
