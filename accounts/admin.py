30bf5ac5d04eaa3da98204fd2f99ed16f982522d
ParisaAg <parisaaghaei17@gmail.com>
2025-07-29 02:07:10 +0330

    ADD features to check online user by redis  and show on admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -17,3 +17,30 @@ admin.site.register(User, UserAdmin)
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'birth_date')
+
+
+
+
+def online_users_view(request):
+    online_keys = cache.keys('online-user-*')
+    online_user_ids = [int(key.split('-')[-1]) for key in online_keys]
+    online_users = User.objects.filter(id__in=online_user_ids)
+
+    context = {
+        'title': 'کاربران آنلاین',
+        'online_users': online_users,
+        'online_count': len(online_user_ids),
+    }
+    return render(request, 'admin/online_users.html', context)
+
+original_get_urls = admin.site.get_urls
+
+def get_urls():
+    urls = original_get_urls()
+    custom_urls = [
+        path('online-users/', admin.site.admin_view(online_users_view), name='online-users'),
+    ]
+    return custom_urls + urls
+
+admin.site.get_urls = get_urls
+
