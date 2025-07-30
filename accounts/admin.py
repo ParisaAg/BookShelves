9ec43f9899155b2482b8a96413312b8515514cce
ParisaAg <parisaaghaei17@gmail.com>
2025-07-30 13:39:57 +0330

    remove online user

--- accounts/admin.py
+++ accounts/admin.py
@@ -28,31 +28,4 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 
-def online_users_view(request):
-    five_minutes_ago = timezone.now() - timedelta(minutes=5)
-    online_activities = UserActivity.objects.filter(last_seen__gte=five_minutes_ago)
-    online_users = [activity.user for activity in online_activities]
-
-    context = {
-        'title': 'Online Users',
-        'online_users': online_users,
-        'online_count': len(online_users),
-        # Required for the admin template
-        'has_permission': request.user.is_staff,
-        'site_header': admin.site.site_header,
-        'site_title': admin.site.site_title,
-        'app_list': admin.site.get_app_list(request)
-    }
-    return render(request, 'admin/online_users.html', context)
-
-original_get_urls = admin.site.get_urls
-
-def get_urls():
-    urls = original_get_urls()
-    custom_urls = [
-        path('online-users/', admin.site.admin_view(online_users_view), name='online-users'),
-    ]
-    return custom_urls + urls
-
-admin.site.get_urls = get_urls
 
