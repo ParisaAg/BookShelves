a1d458cdde401c4e960c4aa8d73d663fbc374a42
ParisaAg <parisaaghaei17@gmail.com>
2025-08-01 19:11:08 +0330

    check out urls and ordetitem were creared and fixed

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,16 @@
+# orders/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, CheckoutView,PaymentVerificationView
+from .views import OrderViewSet, CheckoutView, PaymentVerificationView
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
+    # این خط در پروژه شما وجود ندارد و باید اضافه شود
     path('checkout/', CheckoutView.as_view(), name='checkout'),
+    
     path('orders/verify/', PaymentVerificationView.as_view(), name='payment-verify'),
+    
     path('', include(router.urls)),
 ]
