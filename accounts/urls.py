3eb78ee5532b5c99a8a1b22799bb7fde1ff626f6
ParisaAg <parisaaghaei17@gmail.com>
2025-08-01 17:01:44 +0330

    change account url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,17 @@
-
-from django.urls import path,include
+# accounts/urls.py
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView,LoginView,ProfileView,LogoutView,CheckAuthView,AddressViewSet
-
+from .views import (
+    RegisterView,
+    LoginView,
+    ProfileView,
+    LogoutView,
+    CheckAuthView,
+    AddressViewSet
+)
 
 router = DefaultRouter()
 router.register(r'addresses', AddressViewSet, basename='address')
-
-
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -15,8 +19,6 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='user-profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('check-auth/', CheckAuthView.as_view(), name='check-auth'),
-    path('', include(router.urls)),
-
+        path('', include(router.urls)),
 ]
-
 
