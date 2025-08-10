ff102656c9bae4936a39e2371f69c87a30e3f83b
ParisaAg <parisaaghaei17@gmail.com>
2025-08-10 09:50:51 +0330

    back to latest url

--- books/urls.py
+++ books/urls.py
@@ -1,28 +1,41 @@
-# books/urls.py
-from django.urls import path, include
-from rest_framework_nested import routers
-from .views import (
-    BookViewSet, CategoryViewSet, AuthorViewSet, DiscountViewSet,
-    LatestBooksView, TrendingBooksView, TopSellersView, PopularCategoriesView
-)
-from reviews.views import ReviewViewSet
+# your_app/urls.py
 
-router = routers.SimpleRouter()
-router.register(r'', BookViewSet, basename='book') 
+from django.urls import path, include
+from rest_framework_nested import routers 
+from rest_framework.routers import DefaultRouter
+from reviews.views import ReviewViewSet
+from .views import (
+    BookViewSet,
+    CategoryViewSet,
+    AuthorViewSet,
+    LatestBooksView,
+    TrendingBooksView,
+    TopSellersView,
+    DiscountViewSet,
+    PopularCategoriesView ,
+  
+)
+
+router = DefaultRouter()
+
+router.register(r'books', BookViewSet, basename='book')
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'authors', AuthorViewSet, basename='author')
 router.register(r'discounts', DiscountViewSet, basename='discount')
-
-books_router = routers.NestedSimpleRouter(router, r'', lookup='slug')
+books_router = routers.NestedSimpleRouter(router, r'books', lookup='book')
 books_router.register(r'reviews', ReviewViewSet, basename='book-reviews')
 
 urlpatterns = [
+
     path('', include(router.urls)),
     path('', include(books_router.urls)),
+
+
     
-    # These paths create the URLs you are trying to access
     path('latest-books/', LatestBooksView.as_view(), name='latest-books'),
     path('trending-books/', TrendingBooksView.as_view(), name='trending-books'),
     path('top-sellers/', TopSellersView.as_view(), name='top-sellers'),
-    path('popular-categories/', PopularCategoriesView.as_view(), name='popular-categories'),
+    path('popular-cat/', PopularCategoriesView.as_view(), name='popular-cat'),
+
+    
 ]
