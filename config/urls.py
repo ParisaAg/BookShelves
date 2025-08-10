420b59462a147bafe7773753be928a8289db3763
ParisaAg <parisaaghaei17@gmail.com>
2025-08-10 09:42:02 +0330

    Add NEW

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')), 
-    path('api/books/', include('books.urls')),
+    path('api/', include('books.urls')),
     path('api/', include('orders.urls')),
     path('api/', include('carts.urls')),
     path('postgresql/', include('postgresql_app.urls')),
