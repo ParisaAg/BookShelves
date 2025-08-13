38d786f11bf571342829c326f0d3035dcb98514c
ParisaAg <parisaaghaei17@gmail.com>
2025-08-13 14:18:11 +0330

    check book url and lookup field is based on pk id

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,5 @@
-# your_app/urls.py
-
 from django.urls import path, include
-from rest_framework_nested import routers 
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from reviews.views import ReviewViewSet
 from .views import (
     BookViewSet,
@@ -12,30 +9,25 @@ from .views import (
     TrendingBooksView,
     TopSellersView,
     DiscountViewSet,
-    PopularCategoriesView ,
-  
+    PopularCategoriesView,
 )
 
+# Main router for books
 router = routers.SimpleRouter()
-router.register(r'', BookViewSet, basename='book') 
+router.register(r'', BookViewSet, basename='book')  # Books list & detail by ID
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'authors', AuthorViewSet, basename='author')
 router.register(r'discounts', DiscountViewSet, basename='discount')
 
-books_router = routers.NestedSimpleRouter(router, r'', lookup='slug')
+books_router = routers.NestedSimpleRouter(router, r'', lookup='pk')
 books_router.register(r'reviews', ReviewViewSet, basename='book-reviews')
 
 urlpatterns = [
-
     path('', include(router.urls)),
     path('', include(books_router.urls)),
 
-
-    
     path('latest-books/', LatestBooksView.as_view(), name='latest-books'),
     path('trending-books/', TrendingBooksView.as_view(), name='trending-books'),
     path('top-sellers/', TopSellersView.as_view(), name='top-sellers'),
     path('popular-cat/', PopularCategoriesView.as_view(), name='popular-cat'),
-
-    
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,27 @@
-
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')), 
-    path('api/', include('books.urls')),
-    path('api/', include('orders.urls')),
-    path('api/', include('carts.urls')),
-    path('postgresql/', include('postgresql_app.urls')),
-    path('api/sliders/', include('sliders.urls')),
-    path('api/extra/', include('extra.urls')),
-    path('api/contact/', include('contact.urls')),
-    path('api/', include('wishlist.urls')),
-    path('api/', include('blogs.urls')),
+
+    path('api/auth/', include('accounts.urls')),
+
+    path('api/books/', include('books.urls')),
+
+    path('api/orders/', include('orders.urls')),
+
     
+    path('api/carts/', include('carts.urls')),
+
+    path('postgresql/', include('postgresql_app.urls')),
+
+    path('api/sliders/', include('sliders.urls')),
+
+    path('api/extra/', include('extra.urls')),
+
+    path('api/contact/', include('contact.urls')),
+
+    path('api/wishlist/', include('wishlist.urls')),
+
+    path('api/blogs/', include('blogs.urls')),
 ]
