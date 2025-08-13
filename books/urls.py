be2cf4792bcdd998bde56e2ffbd76771db816a13
ParisaAg <parisaaghaei17@gmail.com>
2025-08-13 14:23:47 +0330

    check book nested router

--- books/urls.py
+++ books/urls.py
@@ -23,11 +23,14 @@ books_router = routers.NestedSimpleRouter(router, r'', lookup='pk')
 books_router.register(r'reviews', ReviewViewSet, basename='book-reviews')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(books_router.urls)),
+
 
     path('latest-books/', LatestBooksView.as_view(), name='latest-books'),
     path('trending-books/', TrendingBooksView.as_view(), name='trending-books'),
     path('top-sellers/', TopSellersView.as_view(), name='top-sellers'),
     path('popular-cat/', PopularCategoriesView.as_view(), name='popular-cat'),
+
+    # Include the main and nested routers
+    path('', include(router.urls)),
+    path('', include(books_router.urls)),
 ]
